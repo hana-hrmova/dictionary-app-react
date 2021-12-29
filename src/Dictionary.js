70bd8651e63a7a99d8abf5e603e7d8bcf218bfd3
hana-hrmova <hrmova.h@gmail.com>
2021-12-29 20:19:45 +0100

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 
@@ -7,10 +8,16 @@ export default function Dictionary() {
     event.preventDefault();
     setKeyword(event.target.value);
   }
+  function handleResponse(response) {
+    console.log(response);
+  }
   function handleSubmit(event) {
     event.preventDefault();
     alert(`Searching for definition of "${keyword}"`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   return (
     <div className="Dictionary">
       <h1>Dictionary App</h1>
