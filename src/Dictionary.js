a0dde5e611398afae3b01dcd047d77fbf7f79eb0
hana-hrmova <hrmova.h@gmail.com>
2022-01-01 17:33:39 +0100

    Started to work on the Photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 
 import DictionaryData from "./DictionaryData";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   const [result, setResult] = useState(null);
+  const [photos, setPhotos] = useState(null);
   function handleChange(event) {
     event.preventDefault();
     setKeyword(event.target.value);
@@ -15,8 +17,19 @@ export default function Dictionary() {
     console.log(response.data[0]);
     setResult(response.data[0]);
   }
+  function handlePhotos(response) {
+    console.log(response);
+    setPhotos(response);
+  }
+  function search() {
+    let pexelsKey = "563492ad6f917000010000018b0d814bee984cf196fdad439afe3474";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelResponse);
+  }
   function handleSubmit(event) {
     event.preventDefault();
+    search();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -36,6 +49,7 @@ export default function Dictionary() {
         </button>
       </form>
       <DictionaryData data={result} />
+      <Photos photos={photos} />
     </div>
   );
 }
