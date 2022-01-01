5925ad4605b9e3556f32793ae38daa8747432057
hana-hrmova <hrmova.h@gmail.com>
2022-01-01 21:02:11 +0100

    Added the photos without proper styiling

--- src/Photos.js
+++ src/Photos.js
@@ -1,5 +1,31 @@
 import React from "react";
+import "./Results.css";
+import "./Photos.css";
 
-export default function Photos() {
-  return <p>Photos</p>;
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <div className="container section">
+        <div className="row">
+          <div className="Photos">
+            {props.photos.data.photos.map(function (photos, index) {
+              return (
+                <div key={index}>
+                  <div className="col-4">
+                    <img
+                      src={photos.src.tiny}
+                      alt={photos.src.original}
+                      className="img-fluid"
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
